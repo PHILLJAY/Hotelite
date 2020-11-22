b096ba6e4aac3ac0c087293b4ea89d9cf2096698
PHILLJAY <philip.jasionowski@gmail.com>
2020-11-22 16:16:14 -0500

    created hotelGet

--- hotelTemplate.php
+++ hotelTemplate.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html>
@@ -10,11 +9,18 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="src/styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&family=Roboto:wght@300&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
+
+    <?php
+    $user = 'root';
+    $password = ''; //To be completed if you have set a password to root
+    $database = 'hotels'; //To be completed to connect to a database. The database must exist.
+    $port = NULL; //Default must be NULL to use default port
+    $mysqli = mysqli_connect('127.0.0.1', $user, $password, $database);
+    ?>
     <script src="" async defer></script>
     <div class="headerTitle">
         <a href="main.html">
@@ -30,7 +36,13 @@
                 <img src="src/img/1-1.jpg" alt="SKARNERGAMING" style="width: 100%;">
             </div>
             <div class="coolInfo">
-                <p class="hotelTitle">La Quinta Inn & Suites</p>
+                <p class="hotelTitle">
+                    <?php
+                    $sql = "SELECT * FROM hotels";
+
+
+                    ?>
+                </p>
                 <p class="perNight"><span class="priceTag">$399</span> Per Night </p>
                 <span></span>
                 <ul class="hotelDetails">
